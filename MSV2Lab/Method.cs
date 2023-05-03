3cf7cefa3ac248d92daebb0796f50910eec12aad
Dmytrosa <256Dmytro@gmail.com>
2023-05-03 22:06:47 +0300

    2Lab

---
+++ MSV2Lab/Method.cs
@@ -0,0 +1,81 @@
+﻿
+using System;
+
+namespace NumericMethodsEquMSV
+{
+    public static class Method
+    {
+        public static double epsilon = 10e-5;
+        public static int ApostRelax = 0, ApostNewton = 0;
+        public static double Fu(double x)
+        {
+            return (Math.Pow(x, 2) + 5 * Math.Sin(x) - 1); ;
+        }
+        public static double Fu_App(double x)
+        {
+            return (2 * x + 5 * Math.Cos(x));
+        }
+        public static double Fu_App2(double x)
+        {
+            return (2 - 5 * Math.Cos(x));
+        }
+        public static double FiFu(double x)
+        {
+            if (x >= 3)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            double fi = Math.Asin((-Math.Pow(x, 2) + 1) / 5);
+
+           
+
+
+            return fi;
+        }
+        public static double FiFuApp(double x)
+        {
+            double fi_der = -(5 * Math.Cos(x) / 2 * Math.Sqrt(-5 * Math.Sin(x) + 1));
+            return 0;
+        }
+        public static double Relaxation(double tau, double x0)
+        {
+            double x = x0;
+
+            do
+            {
+                x0 = x;
+                x = x0 + tau * Fu(x0);
+                ApostRelax++;
+
+            }
+
+            while (Math.Abs(x - x0) > epsilon);
+            return Math.Round(x, 3);
+        }
+        public static double MNewton(double a, double b)
+        {
+            double curr, next, x0;
+            if (Fu(a) * Fu_App2(a) > 0) x0 = a;
+            else x0 = b;
+            next = x0;
+            double funcDer = Fu_App(x0);
+            do
+            {
+                curr = next;
+                next = curr - (Fu(curr) / funcDer);
+                ApostNewton++;
+            } while (Math.Abs(next - curr) > epsilon);
+            return next;
+        }
+        public static int ApriorRelax(double q, double z)
+        {   
+            if (q == 0)
+            {
+                throw new DivideByZeroException("You can't divide by Zero!");
+            }
+            int x = (int)(Math.Log(Math.Abs(z) / epsilon) / Math.Log(1 / q)) + 1;
+
+            return x;
+        }
+    }
+}
---
+++ MSV2Lab/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NumericMethodsEquMSV
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("ModN method: x = " + Method.MNewton(-2.5, 0).ToString("0.000000"));
+            Console.WriteLine("Aposteriori: n = " + Method.ApostNewton);
+            Console.WriteLine("Relax method: x = " + Method.Relaxation(0.13, -2.1));//(-2.5; -2.0)
+            Console.WriteLine("Aposteriori assessment: n = " + Method.ApostRelax);
+            Console.WriteLine("Apriori assessment: n = " + Method.ApriorRelax(0.2, -2.1));
+            Console.ReadKey();
+        }
+    }
+}
